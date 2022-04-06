d788fed01c06fdabfeb4ea17cc2b82fbd2bba003
EstebanLes <estebanles22@gamail.com>
2022-04-06 12:00:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/practicakotlin/practica.kt
@@ -0,0 +1,17 @@
+package com.example.practicakotlin
+
+fun main (){
+    val a = 10
+    val b = 14
+    println( "El valor de A es:$a \nEl valor de B es:$b \nResultado ${suma(a, b)}")
+}
+
+fun suma(a: Int, b:Int):Int {
+return a + b
+}
+
+
+
+
+
+
